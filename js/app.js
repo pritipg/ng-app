4158ea8293c51f470641ec0e8b09e6bf8f7edebc
Priti <pripra3014@gmail.com>
2016-07-05 14:33:52 +0530

    write review

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 (function(){
     var app = angular.module('store', []);
 
-    app.controller('StoreController', function() {
+    app.controller('StoreController', function(){
         this.products = gems;
 
     });
 
-    app.controller('PanelController', function() {
+    app.controller('PanelController', function(){
         this.tab = 1;
 
-        this.selectTab = function(setTab) {
+        this.selectTab = function(setTab){
             this.tab = setTab;
         };
-        this.isSelected = function(checkTab) {
+        this.isSelected = function(checkTab){
             return this.tab === checkTab;
         };
     });
@@ -22,6 +22,15 @@
 
         this.setCurrent = function(imageNumber){
             this.current = imageNumber || 0;
+        };
+    });
+
+    app.controller('ReviewController', function(){
+        this.review = {};
+        this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
         };
     });
 
